5fd11aeb1d0c881b3e58cc238db291bcfb92fc74
fredie7385 <mukii7385@gmail.com>
2023-11-29 09:51:12 +0300

    tod list added

---
+++ src/components/Counter.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import '../styles/Counter.css'
+
+
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    function increment(){
+        setCount(count + 1);
+    }
+    function decrement(){
+        setCount(count -1);
+    }
+    
+  return (
+    <div>
+        <div className="container">
+            <h1>Counter</h1>
+            <p>This is a simple example of a React component.</p>
+            <div>
+            <button onClick={increment}>+</button>
+                <h3><span>{count}</span></h3>
+                <button onClick={decrement}>-</button>           
+           
+            </div>                
+            
+        </div>
+    </div>
+  )
+}
+
+export default  Counter;
